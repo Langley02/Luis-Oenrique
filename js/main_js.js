714ec8e47d17ab5524290f0944d18cb8efbd20e2
Langley02 <spaynblack@gmail.com>
2020-03-11 16:48:29 +0200

    Закончил верстку блоков

--- js/main_js.js
+++ js/main_js.js
@@ -8,12 +8,27 @@ $(document).ready(function () {
         items: 1,
     })
 
-    $("a.nav-link").click(function() {
+    $("a.nav-link").click(function () {
         var elementClick = $(this).attr("href")
         var destination = $(elementClick).offset().top;
         jQuery("html:not(:animated),body:not(:animated)").animate({
             scrollTop: destination
         }, 800);
         return false;
+    });
+
+    var btn = $('#back_to_top');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 1000) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, '300');
     });
 });
